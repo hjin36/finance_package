865a67ee39915fda667010c029137c8f91a3301b
jinhaotian <hjin36@emory.edu>
2018-03-30 19:57:55 -0400

    added account

--- financial_institute.py
+++ financial_institute.py
@@ -6,8 +6,11 @@ class Bank(Institute):
 	pass
 
 class Retail_Bank(Institute):
+	def 
 	pass
 
+class Checking_Account(Debt):
+	def __init__(self,
 
 if __name__ == "__main__":
 	wells_fargo = Retail_Bank("wells fargo")
