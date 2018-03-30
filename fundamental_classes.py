c24fd5608e5fb102071e26cd3469a06a6d064f1c
jinhaotian <hjin36@emory.edu>
2018-03-30 11:44:02 -0400

    added entity,person,institute,product,debt,raisedebt,repay

---
+++ fundamental_classes.py
@@ -0,0 +1,67 @@
+#! /usr/bin/env python3
+
+class Entity:
+	def __init__(self,name,capital=0):
+		self.name = name
+		self.capital = capital
+		self.liquidity = capital
+		self.asset=[]
+		self.liability=[]
+	def raiseDebt(self,debtname,investor,product_id=0,size=0,maturity_days=1,interest=0.00):
+		new_debt = Debt(debtname,self,investor,product_id,size,maturity_days,interest)
+		self.liability.append(new_debt)
+		investor.asset.append(new_debt)
+		self.capital += size
+		self.liquidity -= size
+
+class Person(Entity):
+	def __init__(self,name,capital=0,person_id=0):
+		Entity.__init__(self,name,capital)
+		self.id = person_id
+	def __str__(self):
+		return "Person: %s, Capital: %.2f" % (self.name, self.capital)
+
+class Institute(Entity):
+	def __init__(self,name,capital=0,company_id=0):
+		Entity.__init__(self,name,capital)
+		self.id = company_id
+	def __str__(self):
+		return "Institute: %s, Capital: %.2f" % (self.name, self.capital)
+
+class Product:
+	def __init__(self,name,issuer,investor,product_id=0,size=0):
+		self.name = name
+		self.issuer = issuer
+		self.investor = investor
+		self.id = product_id
+		self.size = size
+	def __str__(self):
+		return "Product: %s, %s get %.2f from %s" % (self.name, self.issuer.name, self.size, self.investor.name)
+
+class Debt(Product):
+	def __init__(self,name,issuer,investor,product_id=0,size=0,maturity_days=1,interest=0.00):
+		Product.__init__(self,name,issuer,investor,product_id,size)
+		self.maturity_days = maturity_days
+		self.interest = interest
+	def __str__(self):
+		return "Debt: %s, %s raised %.2f from %s, maturity %d days, interest: %.5f" % (self.name,self.issuer.name,self.size,self.investor.name,self.maturity_days, self.interest)
+	def __repr__(self):
+		return "%s: %s" % (self.name,self.investor)
+	def repay(self,size):
+		self.size -= size
+		self.issuer.capital -= size
+		self.investor.capital += size
+		if self.size == 0:
+			self.issuer.liability.remove(self)
+			self.investor.asset.remove(self)
+
+
+
+
+if __name__ == "__main__":
+	tom = Person("Tom",10000)
+	goldman = Institute("Goldman Sachs",10 ** 10)
+	goldman.raiseDebt("IOU",tom,0,10000,60,0.1)
+	goldman.liability[0].repay(10000)
+	print(goldman.liability)
+	print(tom.asset)
